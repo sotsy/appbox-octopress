dbb8d86bddb77233751ec9ae4efd68438902937f
Philipp Jaeckel <mail@sots.name>
2013-12-27 16:14:29 +0100

    Solved an issue with iPad screenshots in appbox screenshot view. Arrays of Universal/iPhone apps differ from iPad only apps

--- templates/plugins/appbox.rb
+++ templates/plugins/appbox.rb
@@ -16,7 +16,7 @@ module Jekyll
       super
       @result = Hash.new
 
-      @resultarray = ['icon', 'screenshots', 'name', 'url', 'version', 'price', 'developer', 'developerurl','rating']
+      @resultarray = ['icon', 'name', 'url', 'version', 'price', 'developer', 'developerurl','rating','screenshots']
 
       @cachefolder = File.expand_path "../.appboxcache", File.dirname(__FILE__)
       FileUtils.mkdir_p @cachefolder
@@ -87,7 +87,7 @@ module Jekyll
       screenshots = ''
 
       screenshots += "<div class=\"screenshots\"><div class=\"slider\"><ul>"
-      unless @result["screenshots"].empty?
+      unless @result["screenshots"].nil?
         @result["screenshots"].each do |screenshot|
           screenshots += "<li><img src=\"#{screenshot}\" /></li>"
         end
@@ -113,11 +113,18 @@ module Jekyll
     end
 
     def apple_fetch_data(app_id)
-      requestarray = ['artworkUrl512','screenshotUrls','trackName','trackViewUrl','version','formattedPrice','artistName','artistViewUrl', 'averageUserRatingForCurrentVersion']
+      requestarray = ['artworkUrl512','trackName','trackViewUrl','version','formattedPrice','artistName','artistViewUrl', 'averageUserRatingForCurrentVersion']
 
       url = "http://itunes.apple.com/lookup?id=#{app_id}"
       resp = Net::HTTP.get_response(URI.parse(url))
       app_data = JSON.parse resp.body
+
+      if app_data["results"][0].has_key?("ipadScreenshotUrls")
+        requestarray << "ipadScreenshotUrls"
+      else
+        requestarray << "screenshotUrls"
+      end
+
 
       @resultarray.zip(requestarray).each do |result,request|
         @result[result] = app_data["results"][0][request]
@@ -133,24 +140,24 @@ module Jekyll
 
       # requestarray hints:
       # 1) App Icon e.g. big icon in Chrome Webstore
-      # 2) Array of screenshots
-      # 3) Name of the app
-      # 4) URL to the app
-      # 5) Versionnumber of the app
-      # 6) Price of the app
-      # 7) Developername
-      # 8) Developer URL
-      # 9) Rating value
+      # 2) Name of the app
+      # 3) URL to the app
+      # 4) Versionnumber of the app
+      # 5) Price of the app
+      # 6) Developername
+      # 7) Developer URL
+      # 8) Rating value
+      # 9) Array of screenshots
 
       requestarray = [ doc.css("div.cover-container img").first['src'],
-                        doc.css("div.thumbnails img.screenshot").collect {|thumb| thumb['src']},
                         doc.css("div.info-container div.document-title div").first.content,
                         base_url + "/store/apps/details?id=#{app_id}",
                         doc.css("div[itemprop=softwareVersion]").first.content.strip,
                         doc.css("meta[itemprop=price]").first['content'],
                         doc.css("div.info-container div a.document-subtitle").first.content,
                         base_url + doc.css("div.info-container div a.document-subtitle").first['href'],
-                        doc.css("div.tiny-star div.current-rating").first['style'][/\d+\.\d+/].to_f.round(1) / 100 * 5
+                        doc.css("div.tiny-star div.current-rating").first['style'][/\d+\.\d+/].to_f.round(1) / 100 * 5, 
+                        doc.css("div.thumbnails img.screenshot").collect {|thumb| thumb['src']}
                          ]
       @resultarray.zip(requestarray).each do |result, request|
         @result[result] = request
